004309d2b2c2582feb56d6c9e4984939aa20e4a2
Unknown <den.kir@live.nl>
2018-04-17 09:30:07 +0200

    NEW news.php | UPDATE css/index.php

--- index.php
+++ index.php
@@ -32,12 +32,46 @@
 		</div>
 	</div>
 	
-	<div id="topButton">
+	<div class="news-container" id="news">
+		<h1>Nieuws</h1>
+		<div class="news-scroll-box">
+			<div class="news-article">
+				<h2>Test Titel 1</h2>
+				<p>Quisque lobortis leo et lectus sollicitudin, vel mattis augue venenatis. Mauris rutrum vulputate quam, et venenatis lectus dapibus vel. Sed rutrum enim magna. Nullam eget libero non lacus maximus posuere vel nec dui. Morbi consectetur diam eu aliquam suscipit. Nullam sed mauris quis odio malesuada fringilla. Integer lacus justo, gravida quis magna eget, accumsan consequat dui. Maecenas blandit purus ut venenatis malesuada. Praesent ac lacus mollis, porta eros eget, pretium libero. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst. Donec vel erat sit amet neque maximus accumsan. Pellentesque porta tempor dui, et lobortis diam mattis a. Suspendisse at pretium enim, eget dapibus purus. </p>
+				
+			</div>
+			<div class="news-article">
+				<h2>Test Titel 1</h2>
+				<p>Quisque lobortis leo et lectus sollicitudin, vel mattis augue venenatis. Mauris rutrum vulputate quam, et venenatis lectus dapibus vel. Sed rutrum enim magna. Nullam eget libero non lacus maximus posuere vel nec dui. Morbi consectetur diam eu aliquam suscipit. Nullam sed mauris quis odio malesuada fringilla. Integer lacus justo, gravida quis magna eget, accumsan consequat dui. Maecenas blandit purus ut venenatis malesuada. Praesent ac lacus mollis, porta eros eget, pretium libero. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst. Donec vel erat sit amet neque maximus accumsan. Pellentesque porta tempor dui, et lobortis diam mattis a. Suspendisse at pretium enim, eget dapibus purus. </p>
+				
+			</div>
+			<div class="news-article">
+				<h2>Test Titel 1</h2>
+				<p>Quisque lobortis leo et lectus sollicitudin, vel mattis augue venenatis. Mauris rutrum vulputate quam, et venenatis lectus dapibus vel. Sed rutrum enim magna. Nullam eget libero non lacus maximus posuere vel nec dui. Morbi consectetur diam eu aliquam suscipit. Nullam sed mauris quis odio malesuada fringilla. Integer lacus justo, gravida quis magna eget, accumsan consequat dui. Maecenas blandit purus ut venenatis malesuada. Praesent ac lacus mollis, porta eros eget, pretium libero. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst. Donec vel erat sit amet neque maximus accumsan. Pellentesque porta tempor dui, et lobortis diam mattis a. Suspendisse at pretium enim, eget dapibus purus. </p>
+				
+			</div>
+			<div class="news-article">
+				<h2>Test Titel 1</h2>
+				<p>Quisque lobortis leo et lectus sollicitudin, vel mattis augue venenatis. Mauris rutrum vulputate quam, et venenatis lectus dapibus vel. Sed rutrum enim magna. Nullam eget libero non lacus maximus posuere vel nec dui. Morbi consectetur diam eu aliquam suscipit. Nullam sed mauris quis odio malesuada fringilla. Integer lacus justo, gravida quis magna eget, accumsan consequat dui. Maecenas blandit purus ut venenatis malesuada. Praesent ac lacus mollis, porta eros eget, pretium libero. Suspendisse potenti. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac habitasse platea dictumst. Donec vel erat sit amet neque maximus accumsan. Pellentesque porta tempor dui, et lobortis diam mattis a. Suspendisse at pretium enim, eget dapibus purus. </p>
+				
+			</div>
+		</div>
+	</div>
+	
+	<div id="topButton" >
 		<img src="img/iconscroll2.png" onclick="ScrollTop('.main-nav')">
 	</div>
+	
+	<div class="contact-container">
+		<h1>Contact</h1>
+		<div class="contact-left-box">
+		</div>
+	</div>
+	
 </body>
 
 <footer>
+	
 <script>
 	function ScrollTop(selector) {
         var el = document.querySelector(selector);
