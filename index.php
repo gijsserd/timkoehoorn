bf889240f5df3312195d8ac2d9a33a0449eff071
Unknown <gijsverdonschot@gmail.com>
2018-04-17 12:34:54 +0200

    Album + Single aangepast

--- index.php
+++ index.php
@@ -26,10 +26,34 @@
 		
 		<div class="muziek-sub-container">
 			<div class="album-container">
+			<h2>Nieuwste Album</h2>
 				<div class="album">
+				
+					<img src="img/ihebil.jpg">
+					<div class="album-content">
+						<h3>Nummertjes</h3>
+						<ul>
+							<li>1. <a href="https://open.spotify.com/track/00dLqGsbVdTssDHl4towiN?si=0KUpIeAeTROJhkGdqiaa5Q" target="_blank">Liefdesliedjes</a></li>
+							<li>2. <a href="https://open.spotify.com/track/5X6IXCOlzcJyJcYEUAepYE?si=4lDDVTxoRJGGhF02aKA3YA" target="_blank">In Het Echt Ben Ik Leuker</a></li>
+							<li>3. <a href="https://open.spotify.com/track/1rSTBTRmp3R0nsYJQGZeDu?si=ANIv9XqjT5uqA5aRI4Iq5Q" target="_blank">Voor David</a></li>
+							<li>4. <a href="https://open.spotify.com/track/5O2rOyBVD5dIDXrBKAdUQv?si=qQzjobc_R-qR8B53idKyRA" target="_blank">Ik Uit Mijn Frustratie Middels Alcohol en Popmuziek</a></li>
+							<li>5. <a href="https://open.spotify.com/track/6loTySK5j0Zk0VHbKfePrr?si=OQsfD3JNT1SGB67D3uZUNw" target="_blank">Brief Aan Bart, Pascal en Iris</a></li>
+						</ul>
+					</div>
+				</div>
+				<h2>Nieuwste Single</h2>
+				<div class="single">
 					
+					<img src="img/hgdskommp.jpg">
+					<div class="single-content">
+						<p>rutrum vulputate quam, et venenatis lectus dapibus vel. Sed rutrum enim magna. Nullam eget libero non lacus maximus posuere vel nec dui.
+						Morbi consectetur diam eu aliquam suscipit. Nullam sed mauris quis odio malesuada fringilla. Integer lacus justo, gravida quis magna eget, accumsan consequat dui.
+						Maecenas blandit purus ut venenatis malesuada.
+						</p>
+					</div>
 				</div>
 			</div>
+			
 			<div class="spotify-container">
 				<iframe src="https://open.spotify.com/embed/artist/7y5DunkoStbLnIwfVR91T2" width="400" height="400" frameborder="0" allowtransparency="true"></iframe>
 			</div>
