96d63748af2773600591606d7835ae7b57401e2e
Unknown <gijsverdonschot@gmail.com>
2018-05-28 16:23:12 +0200

    CMS music.php

--- insert_music.php
+++ insert_music.php
@@ -21,30 +21,28 @@ session_start();
 			$upload_directory = "img/";
 			$TargetPath=time().$UploadedFileName;
 			if(move_uploaded_file($_FILES['uploadedimage']['tmp_name'], $upload_directory.$TargetPath)){
-				$prep_stmt = $connect->prepare("INSERT INTO `album`(`name`, `image`, `creation_date`, `spotify`) 
-				VALUES (?, ?, ?, ?)");
-				$prep_stmt->bind_param("ssis", $title, $image, $currentdate, $spotify);
+				$prep_stmt = $connect->prepare("INSERT INTO `album`(`name`, `image`, `spotify`) 
+				VALUES (?, ?, ?)");
+				$prep_stmt->bind_param("sss", $title, $image, $spotify);
 				
 				$title = $_POST["title"]; 
 				$spotify = $_POST["spotifylink"];
 				$image =  $upload_directory.$TargetPath;
-				$currentdate = date('m/d/Y');
 			}
 		}else{
-			$prep_stmt = $connect->prepare("INSERT INTO `album`(`name`, `creation_date`, `spotify`) 
-			VALUES (?, ?, ?)");
-			$prep_stmt->bind_param("ssi", $title, $currentdate, $spotify);
+			$prep_stmt = $connect->prepare("INSERT INTO `album`(`name`, `spotify`) 
+			VALUES (?, ?)");
+			$prep_stmt->bind_param("ss", $title, $spotify);
 			
 			$title = $_POST["title"]; 
 			$spotify = $_POST["spotifylink"];
-			$currentdate = date('m/d/Y');
 		}
 		
 		if ($prep_stmt->execute() === TRUE) {
 			echo "<p>New record created successfully</p>";
 			$prep_stmt->close();
 			$connect->close();
-			header("location: cms_article.php?status=Nieuw_artikel_aangemaakt!");
+			header("location: cms_music.php?status=Nieuw_Album_aangemaakt!");
 		} else {
 			echo "Error: " . $prep_stmt . "<br>" . $conn->error;
 		}
